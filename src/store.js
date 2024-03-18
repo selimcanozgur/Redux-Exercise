79cd9a5dce2d5b30df80109090c8df470227ee22
selimcanozgur <webisdeveloper@gmail.com>
2024-03-18 18:11:18 +0300

    action functions added

--- src/store.js
+++ src/store.js
@@ -39,12 +39,28 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 300 });
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-store.dispatch({ type: "account/payLoan", payload: 300 });
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(300));
+store.dispatch(requestLoan(1000, "Buy a car"));
+store.dispatch(payLoan());
 
 console.log(store.getState());
