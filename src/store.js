ea23db2e6e089ef0976dc27b4795476b0a01906a
selimcanozgur <webisdeveloper@gmail.com>
2024-03-20 14:35:58 +0300

    slices added and new file structure created

--- src/store.js
+++ src/store.js
@@ -1,66 +1,11 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootReducer);
 
-// Mevcut durumu değiştirmesine ve aseknron işlem yapılmasına izin verilmez
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/deposit": {
-      return { ...state, balance: state.balance + action.payload };
-    }
-    case "account/withdraw": {
-      return { ...state, balance: state.balance - action.payload };
-    }
-
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan": {
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    }
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: { amount, purpose },
-  };
-}
-
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(500));
-store.dispatch(withdraw(300));
-store.dispatch(requestLoan(1000, "Buy a car"));
-store.dispatch(payLoan());
-
-console.log(store.getState());
+export default store;
