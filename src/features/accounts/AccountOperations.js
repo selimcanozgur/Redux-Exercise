9ae6bfb3b9a24559d3f6cb012c532c12e0f79ddd
selimcanozgur <webisdeveloper@gmail.com>
2024-03-23 18:25:57 +0300

    redux-thunk used

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -13,13 +13,15 @@ function AccountOperations() {
     loan: loanView,
     loanPurpose: purpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
   const dispatch = useDispatch();
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
@@ -59,7 +61,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting" : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
